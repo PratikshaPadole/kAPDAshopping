a985ca69acacdb24bcc5c6f487e9e815afe9cd08
PratikshaPadole <123239895+PratikshaPadole@users.noreply.github.com>
2023-02-23 13:26:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+var Mainimg=document.getElementsByClassName('mainimg');
+var small=document.getElementsByClassName('smallimg');
+small[0].onclick=function click() {
+    Mainimg.src=small[0].src
+}
+small[1].onclick=function click() {
+    Mainimg.src=small[1].src
+}
+
+small[2].onclick=function click() {
+    Mainimg.src=small[2].src
+}
+
+
